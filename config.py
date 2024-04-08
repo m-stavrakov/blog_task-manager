7df837b8cdc03dfa93a115a315bbc9857f096d98
m-stavrakov <131870370+m-stavrakov@users.noreply.github.com>
2024-04-08 15:24:04 +0100

    Modified requirements file to add SQLAlchemy

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-from sqlalchemy import create_engine
-
-SECRET_KEY = os.getenv('SECRET_KEY', '26e56e8dedecb8cce95b7d4cd2e2ba61e7c1c0dfd63c1657864edb9e7af7decb')
-connection_string = "postgresql://martinstavrakov:password@localhost:5432/blog-tasks"
-engine = create_engine(connection_string, echo=True)
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', connection_string)
