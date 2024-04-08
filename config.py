0dc15f75dc72fd37b7dc79ff0ede1b08597da899
m-stavrakov <131870370+m-stavrakov@users.noreply.github.com>
2024-04-08 18:53:08 +0100

    Another fix in config file, port

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', '26e56e8dedecb8cce95b7d4cd2e2ba61e7c1c0dfd6
 default_connection_string = "postgresql://martinstavrakov:password@localhost:5432/blog-tasks"
 
 if os.environ.get('RENDER'):
-    connection_string = os.environ.get('DATABASE_URL', 'postgresql://martinstavrakov:TSf5xv9xpIBIfvc2bbGmS6hTv81RHgXu@dpg-co9hhmdjm4es73avgd8g-a/fs_blog_tasks')
+    connection_string = os.environ.get('DATABASE_URL', 'postgresql://martinstavrakov:TSf5xv9xpIBIfvc2bbGmS6hTv81RHgXu@dpg-co9hhmdjm4es73avgd8g-a/fs_blog_tasks:5432')
 else:
     connection_string = os.environ.get('DATABASE_URL', default_connection_string)
 
